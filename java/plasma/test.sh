276786fd7f9a7ec5b05956874dd3e161956c578b
kunshang <kunshang.ji@intel.com>
2019-12-26 06:23:14 +0800

    compile options

--- java/plasma/test.sh
+++ java/plasma/test.sh
@@ -34,10 +34,9 @@ pushd ../../cpp
         cmake -DCMAKE_BUILD_TYPE=Release \
             -DCMAKE_C_FLAGS="-g -O3" \
             -DCMAKE_CXX_FLAGS="-g -O3" \
-            -DARROW_BUILD_TESTS=off \
+            -DARROW_BUILD_TESTS=on \
             -DARROW_HDFS=on \
-            -DARROW_BOOST_USE_SHARED=on \
-            -DARROW_PYTHON=on \
+            -DARROW_BOOST_USE_SHARED=off \
             -DARROW_PLASMA=on \
             -DPLASMA_PYTHON=on \
             -DARROW_JEMALLOC=off \
@@ -51,6 +50,6 @@ pushd ../../cpp
     popd
 popd
 
-mvn clean install
-export PLASMA_STORE=$ROOT_DIR/../../cpp/release/release/plasma_store_server
+mvn clean install -Dcheckstyle.skip
+export PLASMA_STORE=$ROOT_DIR/../../cpp/release/release/plasma-store-server
 java -cp target/test-classes:target/classes -Djava.library.path=$ROOT_DIR/../../cpp/release/release/ org.apache.arrow.plasma.PlasmaClientTest
